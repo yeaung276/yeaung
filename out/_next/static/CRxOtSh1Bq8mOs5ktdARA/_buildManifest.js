27ffee4ec2731a5ce3de2cc60c248923feae1c84
yeaung276 <yeyintaung.ya276@gmail.com>
2023-03-28 18:02:45 +0700

    remove unnecessary changes

--- out/_next/static/CRxOtSh1Bq8mOs5ktdARA/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-1cfcf582dd59d29d.js"],"/_error":["static/chunks/pages/_error-a12d0fec618c6e85.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
