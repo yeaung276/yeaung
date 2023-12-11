11d4bbb4750691099024fcea44cce9a5ccf112f0
yeaung276 <yeyintaung.ya276@gmail.com>
2023-12-11 16:25:34 +0700

    fix deployment issue

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,8 +1,8 @@
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
 import React from "react";
 
 interface ItemProps {
-  img?: string;
+  img?: string | StaticImageData;
   title?: string;
   description?: string;
   onClick?: () => void;
