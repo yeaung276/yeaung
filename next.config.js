10c5403b742f7f04fe56ed70993ba34d1393db5d
yeaung276 <yeyintaung.ya276@gmail.com>
2023-12-11 17:03:10 +0700

    fix deployment issue

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/yeaung' : '',
+  assetPrefix: '/yeaung', // process.env.NODE_ENV === 'production' ? '/yeaung' : '',
   images: { unoptimized: true }
 }
 
