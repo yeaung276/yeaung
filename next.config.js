da8e34a95ab69b45c69c3e10f0caa54119d1d735
yeaung276 <yeyintaung.ya276@gmail.com>
2023-12-11 17:00:50 +0700

    fix deployment issue

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export'
+  output: 'export',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/yeaung' : '',
+  images: { unoptimized: true }
 }
 
 module.exports = nextConfig
