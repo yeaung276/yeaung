9d13d6b55549d3b7d11d7b43bc7e6743f1a45ca1
yeaung276 <yeyintaung.ya276@gmail.com>
2023-12-11 16:45:50 +0700

    fix deployment issue

--- app/projects/[projectId]/page.tsx
+++ app/projects/[projectId]/page.tsx
@@ -1,3 +1,14 @@
-export default async function ProjectDetail() {
-  return <>Id</>;
+import { Project, getProjects } from "@/data/fetches";
+
+export async function generateStaticParams() {
+  const projects = await getProjects();
+  return projects.map((x) => ({ projectId: x.md }));
+}
+
+export default function ProjectDetail({
+  params,
+}: {
+  params: { projectId: string };
+}) {
+  return <div>{params.projectId}</div>;
 }
--- src/data/fetches.ts
+++ src/data/fetches.ts
@@ -1,4 +1,6 @@
 
 import {projects} from './dataSource/projects';
 
+export type Project = typeof projects[0]
+
 export const getProjects = async () => projects;
