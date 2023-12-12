3b1fd59504c06c4e75b377eb815c5f1cfc861ab3
yeaung276 <yeyintaung.ya276@gmail.com>
2023-12-12 16:30:26 +0700

    fix on broken images

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -1,17 +1,12 @@
-"use client"
 import NImage from 'next/image'
 import { ComponentProps } from 'react';
+
  
-const imageLoader = ({ src, width, quality }: any) => {
-  const isProd = process.env.NODE_ENV === 'production';
-  return `${isProd ? '/yeaung' : ''}/${src}?w=${width}&q=${quality || 75}`
-}
- 
-export default function Image({...props}: ComponentProps<typeof NImage>) {
+export default async function Image({src, ...props}: ComponentProps<typeof NImage>) {
   return (
     <NImage
-      loader={imageLoader}
-      {...props}
+    src={`${process.env.NODE_ENV === 'production' ? '/yeaung' : ''}${src}`}
+    {...props}
     />
   )
 }
