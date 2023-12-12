454ca68956d2380ebdee66f6fc6cbb37789f20e7
yeaung276 <yeyintaung.ya276@gmail.com>
2023-12-12 16:31:49 +0700

    fix on broken images

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -2,7 +2,7 @@ import NImage from 'next/image'
 import { ComponentProps } from 'react';
 
  
-export default async function Image({src, ...props}: ComponentProps<typeof NImage>) {
+export default function Image({src, ...props}: ComponentProps<typeof NImage>) {
   return (
     <NImage
     src={`${process.env.NODE_ENV === 'production' ? '/yeaung' : ''}${src}`}
